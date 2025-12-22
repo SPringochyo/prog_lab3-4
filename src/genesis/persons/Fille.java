3ca9daaf5e0d3afc727d46f225e2ba139b619496
SPring <springochyo@gmail.com>
2025-12-22 16:29:30 +0300

    mdo

--- src/genesis/persons/Fille.java
+++ src/genesis/persons/Fille.java
@@ -17,6 +17,34 @@ public class Fille extends Person implements Jumper {
     }
 
 
+    public void think(double chance) {
+        if (this.contextIs(Context.SHINY_NOSE)) {
+            if (chance < 0.6) {
+                this.think("Я не в восторге от сравнения носов");
+            } else if (chance < 0.8) {
+                this.think("Я недоволен!");
+            } else if (chance < 0.9) {
+                this.think("Да ну вас!!!");
+            } else {
+                this.think("Зайчик вышел на крыльцо почесать свое лицо");
+            }
+            this.setShine(false);
+            this.setContext(Context.NOT_HAPPY);
+
+        } else if (this.contextIs(Context.NOT_HAPPY)) {
+            if (chance < 0.6) {
+                this.think("Хочу скорее смотаться");
+            } else if (chance < 0.8) {
+                this.think("Закрой за мной дверь, я ухожу!");
+            } else if (chance < 0.9) {
+                this.think("Стоп, мне не приятно!");
+            } else {
+                this.think("Кто куда, а я по сугробам");
+            }
+        }
+    }
+
+
     @Override
     public void jump() {
         System.out.println(this.getName() + " высоко подскочил");
--- src/genesis/persons/Karlson.java
+++ src/genesis/persons/Karlson.java
@@ -6,10 +6,25 @@ public class Karlson extends Person {
 
     public Karlson(String name) {
         super(name);
+        this.setRandomCondition();
     }
 
 
     public void stay(String where) {
         System.out.println(this.getName() + " стоит " + where);
+    }
+
+
+    public void stay(double chance) {
+        if (chance < 0.6) {
+            System.out.println(this.getName() + " стоит " + "за спиной");
+        } else if (chance < 0.8) {
+            System.out.println(this.getName() + " стоит " + "за дверью");
+        } else if (chance < 0.9) {
+            System.out.println(this.getName() + " стоит " + "в королевских покоях");
+        } else {
+            System.out.println(this.getName() + " стоит " + "на коленях");
+        }
+
     }
 }
